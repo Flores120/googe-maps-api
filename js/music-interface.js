ac40ddde91501c8ee25a289530b198b2b0dc5191
Alvaro Flores and Jackson Meyer <alvaro+jackson@>
2016-10-12 16:18:39 -0700

    add pictures to website, any artist

--- js/music-interface.js
+++ js/music-interface.js
@@ -6,10 +6,22 @@ $(document).ready(function(){
    var artist = $('#inputArt').val();
    $.get('http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=' + artist + '&api_key=' + apiKey + '&format=json').then(function(response) {
      console.log(response.results.artistmatches.artist[0]);
-      $('#name').append("Name: " + (response.results.artistmatches.artist[0].name));
-      $('#about').append("website about their music: " + (response.results.artistmatches.artist[0].url));
-      $('#streamable').append("number of songs to stream through Last.fm: " + (response.results.artistmatches.artist[0].streamable));
-      $('#images').append("some cool pictures of " + artist + ": "  +(response.results.artistmatches.artist[0].image[0]));
+      $('#name').text("Name: " + (response.results.artistmatches.artist[0].name));
+      $("#link").attr("href", response.results.artistmatches.artist[0].url);
+      $('#streamable').text("number of songs to stream through Last.fm: " + (response.results.artistmatches.artist[0].streamable));
+
+      console.log(response.results.artistmatches.artist[0].image.length);
+      for(var i = 0; i <= response.results.artistmatches.artist[0].image.length; i++) {
+        console.log(response.results.artistmatches.artist[0].image[i]);
+        $("#images").attr("src", (response.results.artistmatches.artist[0].image[i]["#text"]));
+      }
+
+
+
+
+
+
+
     });
  });
 });
